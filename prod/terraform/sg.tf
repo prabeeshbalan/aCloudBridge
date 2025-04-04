9912fe3089106ac7a7b12b49771713f1e271391f
PrabeeshBalan <prabeesh.balan@gmail.com>
2025-04-04 00:10:35 -0400

    Added port allow for(3000,3001,5000) AWS security group in terraform

--- prod/terraform/sg.tf
+++ prod/terraform/sg.tf
@@ -23,11 +23,25 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_3005" {
   security_group_id = aws_security_group.my-public-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 3005
+  from_port         = 3000
   ip_protocol       = "tcp"
-  to_port           = 3005
+  to_port           = 3000
 }
-
+# inbound rules to all ssh to 22 from everywhere
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_3005" {
+  security_group_id = aws_security_group.my-public-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3001
+  ip_protocol       = "tcp"
+  to_port           = 3001
+}
+# inbound rules to all ssh to 22 from everywhere
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_3005" {
+  security_group_id = aws_security_group.my-public-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5000
+  ip_protocol       = "tcp"
+  to_port           = 5000
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.my-public-sg.id
   cidr_ipv4         = "0.0.0.0/0"
